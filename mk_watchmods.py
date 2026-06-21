2a7bb95417498d2ffcbb33542bf0455c58ebb983
kmavyrle <kmavyrle@gmail.com>
2026-06-21 18:42:14 +0800

    Update mk_watchmods.py

--- mk_watchmods.py
+++ mk_watchmods.py
@@ -502,7 +502,7 @@ window.addEventListener('keydown', e => {
 </html>
 """
 
-with st.expander("🎮 bored? play a game", expanded=False):
+with st.expander("🎮 Tap to play", expanded=False):
     components.html(GAME_HTML, height=180)
 
 st.divider()
